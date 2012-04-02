c93c54e16db8e8fb5bc2d3c819a51b0a396309c1
Grady O'Connell <flipcoder@gmail.com>
2012-04-01 22:05:43 -0700

    Added first addon file

---
+++ addons/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys,os, imp
+
+all_addons = {}
+
+def process_path(path):
+    global base
+    if not os.path.exists(path):
+        return
+    if not os.path.abspath(path) in sys.path:
+        sys.path.append(path)
+
+    for fn in os.listdir(path):
+        if fn.lower().endswith(".py") and not fn.lower() == "__init__.py":
+            all_addons[fn] = __import__(fn[:-3], globals(), locals(), [], -1)
+            #if hasattr(base[fn],"category"): # TODO addon categories
+        elif os.path.isdir(os.path.join(path,fn)):
+            base[fn] = __import__(fn, globals(), locals(), [], -1)
+
+def process():
+    process_path(os.path.dirname(os.path.realpath(__file__)))
+    process_path(os.path.join(os.environ['HOME'], ".siege", "addons"))
+
---
+++ addons/java.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+class Java(object):
+    def __init__(self):
+        print "init"
+        
+

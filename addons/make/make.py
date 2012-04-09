40deff516c14f092a41f58810f781236626b912f
Grady O'Connell <flipcoder@gmail.com>
2012-04-09 12:29:50 -0700

    Added premake support

--- addons/make/make.py
+++ addons/make/make.py
@@ -5,8 +5,18 @@ from sgmake import Status
 from common import Settings
 
 def make(project):
-    return Status.UNSUPPORTED
+    project.makepath = Settings.get('make_path')
+    if project.makepath:
+        if project.makepath[-1] != os.sep and os.altsep and Settings.get('make_path')[-1] != os.altsep:
+            project.makepath += os.sep
+    else:
+        project.makepath = ""
+    
+    os.system("%smake" % project.makepath)
+    return Status.SUCCESS
 
 def compatible(project):
+    if os.path.isfile("Makefile"):
+        return True
     return False
 
--- addons/make/premake.py
+++ addons/make/premake.py
@@ -5,8 +5,42 @@ from sgmake import Status
 from common import Settings
 
 def make(project):
-    return Status.UNSUPPORTED
+    project.makepath = Settings.get('make_path')
+    if project.makepath:
+        if project.makepath[-1] != os.sep and os.altsep and Settings.get('make_path')[-1] != os.altsep:
+            project.makepath += os.sep
+    else:
+        project.makepath = ""
+
+    premake = ""
+    if os.path.isfile("premake4.lua"):
+        premake = "premake4"
+    elif os.path.isfile("premake.lua"):
+        premake = "premake"
+
+    premake_platform = "gmake"
+
+    os.system("%s%s %s" % (project.makepath, premake, premake_platform))
+    return Status.SUCCESS
+
+def update(project):
+    # make sure theres a make step after premake
+    make_step = ("make","make")
+    clean_step = ("clean", "clean")
+    if make_step in project.steps:
+        project.steps.remove(make_step)
+    if clean_step in project.steps:
+        project.steps.remove(clean_step)
+    i = 0
+    for s in project.steps:
+        i += 1
+        if s[0] == "make" and s[1] == "premake":
+            project.steps.insert(i,clean_step)
+            project.steps.insert(i+1, make_step)
+            break
 
 def compatible(project):
+    if os.path.isfile("premake.lua") or os.path.isfile("premake4.lua"):
+        return True
     return False
 

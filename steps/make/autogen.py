671e77a8d0544f91c14c11ccc06a808fc492ec39
flipcoder <flipcoder@gmail.com>
2013-11-29 15:29:07 -0800

    autogen

--- steps/make/autogen.py
+++ steps/make/autogen.py
@@ -1,25 +1,49 @@
 #!/usr/bin/env python
 import os
 import sgmake
+import subprocess
 from common import Status
 from common import Settings
 from common import Support
+from common.Plugin import Plugin
+import clean.clean
 
 def make(project):
-    #try:
-    #    project.makepath = os.path.abspath(os.path.expanduser(Settings.get('make_path')))
-    #except:
-    #    project.makepath = ""
-
-    #os.system(os.path.join(project.makepath,"make"))
+    try:
+        subprocess.check_call(['./autogen.sh'])
+    except subprocess.CalledProcessError:
+        return Status.FAILURE
     return Status.SUCCESS
 
 def update(project):
-    # TODO: reorder make steps so that autogen call is before configure and makefile
-    pass
+    configure_step = Plugin("steps", "make", "configure")
+    make_step = Plugin("steps", "make", "makefile")
+    clean_step = Plugin("steps", "clean", "clean")
+    remove_steps = [
+        configure_step,
+        make_step,
+        clean_step
+    ]
+    
+    for s in remove_steps:
+        if s in project.steps:
+            project.steps.remove(s)
+            
+    i = 0
+    
+    for s in project.steps:
+        if s.type == "make" and s.name == "autogen":
+            if clean.clean.compatible(project) & Support.USER:
+                project.steps.insert(i, clean_step)
+                project.steps.insert(i+2, configure_step)
+                project.steps.insert(i+4, make_step)
+            else:
+                project.steps.insert(i+1, configure_step)
+                project.steps.insert(i+3, make_step)
+        i += 1
 
 def compatible(project):
-    support = Support.ENVIRONMENT | Support.USER | Support.AUTO
+    support = Support.MASK & (~Support.PROJECT)
     if os.path.isfile("autogen.sh"):
         support |= Support.PROJECT
     return support
--- steps/make/configure.py
+++ steps/make/configure.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 import os
+import subprocess
 import sgmake
 from common import Status
 from common import Settings
 from common import Support
 
 def make(project):
-    #try:
-    #    project.makepath = os.path.abspath(os.path.expanduser(Settings.get('make_path')))
-    #except:
-    #    project.makepath = ""
-
-    #os.system(os.path.join(project.makepath,"make"))
+    try:
+        subprocess.check_call(['./configure'])
+    except subprocess.CalledProcessError:
+        return Status.FAILURE
     return Status.SUCCESS
 
 def update(project):
-    # TODO: reorder steps so that configure comes before make
     pass
 
 def compatible(project):
-    support = Support.ENVIRONMENT | Support.USER | Support.AUTO
+    support = Support.MASK & (~Support.PROJECT)
     if os.path.isfile("configure"):
         support |= Support.PROJECT
     return support

befb41a53e8703b27477f78025cdbc7f88967105
Grady O'Connell <flipcoder@gmail.com>
2015-03-23 21:51:25 -0700

    changed tempfile naming

--- steps/make/makefile.py
+++ steps/make/makefile.py
@@ -13,7 +13,8 @@ def make(project):
     # relink a broken tmpfs-based obj dir
     if os.path.islink('obj') and not os.path.exists('obj'):
         os.unlink('obj')
-        os.symlink(tempfile.mkdtemp(), 'obj')
+        prefix = 'sg-' + os.getlogin() + '-' + project.name + '-'
+        os.symlink(tempfile.mkdtemp(prefix=prefix), 'obj')
         
     try:
         project.makepath = os.path.abspath(os.path.expanduser(Settings.get('make_path')))

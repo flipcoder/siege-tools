df8a9fa5a4a6f2bcce4789cebe17b0c4dd3f7e60
Grady O'Connell <flipcoder@gmail.com>
2021-01-26 22:32:22 -0800

    haxe support

---
+++ steps/make/haxe.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+import sgmake
+from common import Status
+from common import Support
+from common import Settings
+from common.Plugin import Plugin
+import subprocess
+from common import call
+
+def make(project):
+
+    cmd = [ 'haxe', 'compile.hxml' ]
+    
+    try:
+        call(cmd)
+    except subprocess.CalledProcessError:
+        return Status.FAILURE
+
+    return Status.SUCCESS
+
+def update(project):
+    pass
+    
+def compatible(project):
+    support = Support.MASK & (~Support.PROJECT)
+    if os.path.isfile("compile.hxml"):
+        support |= Support.PROJECT
+    return support
+
